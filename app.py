f7d95eb0ef3fcdf1de0767d37250e508563b47a5
Aniket Verma <aniket.kurji@gmail.com>
2025-07-19 12:35:28 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 model = pickle.load(open("model.pkl", "rb"))
 
 st.set_page_config(page_title="Salary Predictor")
-st.title( Salary Prediction Web App")
+st.title("Salary Prediction Web App")
 
 experience = st.slider("Years of Experience", 0, 30, 2)
 
