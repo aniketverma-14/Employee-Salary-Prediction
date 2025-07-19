b5cdf79195f7591711c44700cd82bf7e2572b1bc
Aniket Verma <aniket.kurji@gmail.com>
2025-07-19 12:55:33 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import gzip
 with gzip.open("job_change_model.pkl.gz", "rb") as f:
     model = pickle.load(f)
 
-st.set_page_config(page_title="Salary Predictor")
 st.title("Salary Prediction Web App")
 
 experience = st.slider("Years of Experience", 0, 30, 2)
@@ -15,4 +14,7 @@ experience = st.slider("Years of Experience", 0, 30, 2)
 if st.button("Predict Salary"):
     df = pd.DataFrame([[experience]], columns=["experience"])
     prediction = model.predict(df)[0]
-    st.success(f"Estimated Salary: ₹{int(prediction[0]):,}")
+if prediction == 1:
+    st.success("This person is likely to change jobs.")
+else:
+    st.warning("This person is not likely to change jobs.")
